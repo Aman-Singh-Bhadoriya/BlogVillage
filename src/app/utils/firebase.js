cc804d65d37bd606aab5e3afe087df3c072b993b
Aman-Singh-bhadoriya <143498570+Aman-Singh-Bhadoriya@users.noreply.github.com>
2025-04-01 14:43:17 +0530

    Firebase updates

--- src/app/utils/firebase.js
+++ src/app/utils/firebase.js
@@ -8,15 +8,17 @@ const firebaseConfig = {
   apiKey: "AIzaSyDJ2FnVvDRC6xhC0mqFBn6fOslhGD0a24Y",
   authDomain: "village-ca2f7.firebaseapp.com",
   projectId: "village-ca2f7",
-  storageBucket: "village-ca2f7.firebasestorage.app",
+  storageBucket: "village-ca2f7.appspot.com",
   messagingSenderId: "857204853377",
-  appId: "1:857204853377:web:334a7704c3d63f67669b80"
+  appId: "1:857204853377:web:334a7704c3d63f67669b80",
+  measurementId: "G-MEASUREMENT_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-const getAnalytics = getAnalytics(app);
+
+const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { auth, db, storage };
+export { auth, db, storage, analytics };
