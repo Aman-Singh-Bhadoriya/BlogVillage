c9bb92fba1af8b614530e8b300cbcd05687f4ce1
Aman-Singh-bhadoriya <143498570+Aman-Singh-Bhadoriya@users.noreply.github.com>
2025-04-01 14:57:27 +0530

    Done

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+// src/app/page.js
+
+export default function Page() {
+    return (
+      <div>
+        <h1>Welcome to My Next.js Project</h1>
+      </div>
+    );
+  }
+  
--- src/app/utils/firebase.js
+++ src/app/utils/firebase.js
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJ2FnVvDRC6xhC0mqFBn6fOslhGD0a24Y",
@@ -11,12 +11,16 @@ const firebaseConfig = {
   storageBucket: "village-ca2f7.appspot.com",
   messagingSenderId: "857204853377",
   appId: "1:857204853377:web:334a7704c3d63f67669b80",
-  measurementId: "G-MEASUREMENT_ID"
+  measurementId: "G-MEASUREMENT_ID" 
 };
 
 const app = initializeApp(firebaseConfig);
 
-const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
+let analytics;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app); 
+}
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
